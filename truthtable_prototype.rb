c281bc529798f9baf0a2e7e2d5a622335d506054
Tanner Jones <tannerjones10@gmail.com>
2014-10-08 23:50:57 -0400

    Adds working truth table prototype

--- truthtable_prototype.rb
+++ truthtable_prototype.rb
@@ -5,18 +5,23 @@ require 'optparse'
 # A premise used in truth tables
 class Premise
   attr_accessor :symbol, :position
-  @position = 0
-
-  def initialize(premise_symbol)
-    @sybmol = premise_symbol
+  @position
+  def initialize(proposition_symbol)
+    @symbol = proposition_symbol  
   end
 
-  def add_position(position)
-    @position = position
+  def calc_interval
+    2**(@position - 1)
   end
 
   def truth?(iteration)
-   return true 
+    if iteration < calc_interval
+      return true
+    else
+	  multiple = iteration / calc_interval
+	  return false if multiple.odd?
+	  true
+    end
   end
 end
 
@@ -25,26 +30,55 @@ end
 class TruthTable
   attr_reader  :premises
 
-  def initialize(p,q)
+  def initialize(p, q, r)
     @premises = Array.new 
-    premise_p = Premise.new("p")
-	premise_p.add_position 1
-	premise_q = Premise.new("q")
-	premise_q.add_position 2
+    premise_p = Premise.new(p)
+	premise_p.position = 1
+	premise_q = Premise.new(q)
+	premise_q.position = 2
+	premise_r = Premise.new(r)
+	premise_r.position = 3
 	@premises.push premise_p
 	@premises.push premise_q
+	@premises.push premise_r
+  end
+  
+  def calc_iterations
+    2 * (2**(@premises.length-1))
   end
 
-  def draw
-	puts '|  p  |  q  |' #output heading
-	puts '-------------'
-	4.times do |iteration|
-      @premises.each do |premise|
-        premise.truth?(iteration) 
-	  end
+  def generate_underline
+    @premises.length.times { print '------' }
+	puts "-\n" 
+  end
+
+  def generate_headers
+    @premises.each do |premise|
+      print "|  #{premise.symbol}  " 
 	end
+	puts '|'
+	generate_underline
+  end
+  
+  def generate_rows
+    calc_iterations.times do |iteration|
+      @premises.each do |premise|
+        if premise.truth?(iteration)
+          print '|  1  '
+	    else 
+          print '|  0  '
+	    end
+	  end
+	  puts '|'
+      generate_underline
+    end
+  end
+  
+  def draw
+    generate_headers 
+	generate_rows
   end
 end
 
-tt = TruthTable.new("p", "q")
+tt = TruthTable.new("p", "q", "r")
 tt.draw
